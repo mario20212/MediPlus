936c83460f09f5963974a19396e3a65162637198
Aboodtt404 <abdelrahmansat2003@gmail.com>
2023-12-09 16:19:16 +0200

    fixed sql configuration/manage users

--- routes/handleUser.js
+++ routes/handleUser.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/registerController');
+const UserController = require('../controllers/userController');
 
 const Userhandler = new UserController();
 
