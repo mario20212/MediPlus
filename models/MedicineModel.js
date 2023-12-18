62f8c7cf64141dc08302f6657db09f1dfe3ed1eb
imposterjump <125587220+imposterjump@users.noreply.github.com>
2023-12-18 23:54:14 +0200

    starting eav design

--- models/MedicineModel.js
+++ models/MedicineModel.js
@@ -2,109 +2,125 @@ const { query } = require('../database/MySQL-connection');
 const tableName = 'medicine_details';
 
 class MedicineModel {
-  async getRowCount() {
-    try {
-      const [result] = await query('SELECT COUNT(*) as rowCount FROM ??', [tableName]);
+    async getRowCount() {
+        try {
+            const [result] = await query('SELECT COUNT(*) as rowCount FROM ??', [tableName]);
 
-      console.log('Query Response:', result);
+            console.log('Query Response:', result);
 
-      if (!result || typeof result !== 'object') {
-        throw new Error(`Unexpected response from the database for table ${tableName}`);
-      }
+            if (!result || typeof result !== 'object') {
+                throw new Error(`Unexpected response from the database for table ${tableName}`);
+            }
 
-      const rowCount = result.rowCount || 0;
-      console.log(`Row count in ${tableName}: ${rowCount}`);
+            const rowCount = result.rowCount || 0;
+            console.log(`Row count in ${tableName}: ${rowCount}`);
 
-      return rowCount;
-    } catch (error) {
-      console.error('Error getting row count:', error.message);
-      throw error;
+            return rowCount;
+        } catch (error) {
+            console.error('Error getting row count:', error.message);
+            throw error;
+        }
     }
-  }
-  async getLatestMedicine() {
-    try {
-      const [result] = await query('SELECT `Average Review %` FROM ?? LIMIT 1', [tableName]);
+    async getLatestMedicine() {
+        try {
+            const [result] = await query('SELECT `Average Review %` FROM ?? LIMIT 1', [tableName]);
 
-      console.log('Query Response:', result);
+            console.log('Query Response:', result);
 
-      if (!result || typeof result !== 'object') {
-        throw new Error(`Unexpected response from the database for table ${tableName}`);
-      }
+            if (!result || typeof result !== 'object') {
+                throw new Error(`Unexpected response from the database for table ${tableName}`);
+            }
 
-      const averageReviewPercentage = result['Average Review %'];
-      return averageReviewPercentage;
-    } catch (error) {
-      console.error('Error getting the latest medicine:', error.message);
-      throw error;
+            const averageReviewPercentage = result['Average Review %'];
+            return averageReviewPercentage;
+        } catch (error) {
+            console.error('Error getting the latest medicine:', error.message);
+            throw error;
+        }
     }
-  }
 
-  async getFirstNineMedicines() {
-    try {
-      const result = await query('SELECT * FROM ?? LIMIT 9', [tableName]);
+    async getFirstNineMedicines() {
+        try {
+            const result = await query('SELECT * FROM ?? LIMIT 9', [tableName]);
 
-      if (!Array.isArray(result)) {
-        throw new Error(`Unexpected response from the database for table ${tableName}`);
-      }
+            if (!Array.isArray(result)) {
+                throw new Error(`Unexpected response from the database for table ${tableName}`);
+            }
 
-      return result;
-    } catch (error) {
-      console.error('Error getting the first nine medicines:', error.message);
-      throw error;
+            return result;
+        } catch (error) {
+            console.error('Error getting the first nine medicines:', error.message);
+            throw error;
+        }
     }
-  }
-  async getAllMedicines() {
-    try {
-      const result = await query('SELECT * FROM ??', [tableName]);
+    async getAllMedicines() {
+        try {
+            const result = await query('SELECT * FROM ??', [tableName]);
 
-      if (!Array.isArray(result)) {
-        throw new Error(`Unexpected response from the database for table ${tableName}`);
-      }
+            if (!Array.isArray(result)) {
+                throw new Error(`Unexpected response from the database for table ${tableName}`);
+            }
 
-      return result;
-    } catch (error) {
-      console.error('Error getting all medicines:', error.message);
-      throw error;
+            return result;
+        } catch (error) {
+            console.error('Error getting all medicines:', error.message);
+            throw error;
+        }
     }
-  }
 
-  async getMedicineById(medicineId) {
-    try {
-      const [result] = await query('SELECT * FROM ?? WHERE id = ?', [tableName, medicineId]);
+    async getMedicineById(medicineId) {
+        try {
+            const [result] = await query('SELECT * FROM ?? WHERE id = ?', [tableName, medicineId]);
 
-      console.log('Query Response:', result);
+            console.log('Query Response:', result);
 
-      if (!result) {
-        console.error(`No medicine found with ID ${medicineId}`);
-        return null;
-      }
+            if (!result) {
+                console.error(`No medicine found with ID ${medicineId}`);
+                return null;
+            }
 
-      const medicine = result;
+            const medicine = result;
 
-      return medicine;
-    } catch (error) {
-      console.error(`Error getting medicine with ID ${medicineId}:`, error.message);
-      throw error;
+            return medicine;
+        } catch (error) {
+            console.error(`Error getting medicine with ID ${medicineId}:`, error.message);
+            throw error;
+        }
     }
-  }
 
-  async getMedicinesByUses(uses, excludedId) {
-    try {
-      const queryString = 'SELECT * FROM ?? WHERE Uses LIKE ? AND id != ? LIMIT 8';
-      const escapedValues = [tableName, uses, excludedId];
-      const [result] = await query(queryString, escapedValues);
-    
-      console.log(`Medicines with uses ${uses} excluding ID ${excludedId}:`, result);
-    
-      return result;
-    } catch (error) {
-      console.error(`Error getting medicines by uses:`, error.message);
-      throw error;
+    async getMedicinesByUses(uses, excludedId) {
+        try {
+            const queryString = 'SELECT * FROM ?? WHERE Uses LIKE ? AND id != ? LIMIT 8';
+            const escapedValues = [tableName, uses, excludedId];
+            const [result] = await query(queryString, escapedValues);
+
+            console.log(`Medicines with uses ${uses} excluding ID ${excludedId}:`, result);
+
+            return result;
+        } catch (error) {
+            console.error(`Error getting medicines by uses:`, error.message);
+            throw error;
+        }
     }
-  }
-  
+    async getMedicineOptionsById(medicineId) {
+        try {
+            const optionsQuery = `
+        SELECT o.option_name, mo.value
+        FROM options o
+        INNER JOIN medicine_options mo ON o.id = mo.option_id
+        WHERE mo.medicine_id = ?;
+      `;
 
-  
+            const result = await query(optionsQuery, [medicineId]);
+            return result;
+        } catch (error) {
+            console.error('Error retrieving medicine options:', error);
+            throw error;
+        }
+    }
+
+
+
 
 
 
@@ -113,6 +129,3 @@ class MedicineModel {
 }
 
 module.exports = MedicineModel;
-
-
-
--- models/order.js
+++ models/order.js
@@ -1,31 +1,36 @@
 const mysql = require('mysql2');
 
 class Order {
-    constructor(id, useremail, products, productsquantity, prices, total_price, payment_method, address, phone_number) {
+    constructor(id, userid, useremail, products, productsquantity, total_price, payment_method, address, phone_number, first_name, last_name) {
         this.id = id;
+        this.userid = userid;
         this.useremail = useremail;
         this.products = products;
         this.productsquantity = productsquantity;
-        this.prices = prices;
         this.total_price = total_price;
         this.payment_method = payment_method;
         this.address = address;
         this.phone_number = phone_number;
+        this.first_name = first_name;
+        this.last_name = last_name;
     }
 
     // Create an order
-    async createOrder(id, useremail, products, productsquantity, prices, total_price, payment_method, address, phone_number) {
+    async createOrder(id, userid, useremail, products, productsquantity, total_price, payment_method, address, phone_number, first_name, last_name) {
         try {
-            const query = ('INSERT INTO orders (useremail, products, productsquantity, prices, total_price, payment_method, address, phone_number) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [id, useremail, products, productsquantity, prices, total_price, payment_method, address, phone_number]);
+            const query = ('INSERT INTO orders (userid,useremail, products, productsquantity, total_price, payment_method, address, phone_number) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [id, userid, useremail, products, productsquantity, prices, total_price, payment_method, address, phone_number]);
             await connection.promise().execute(query, [
+                this.userid,
                 this.useremail,
                 JSON.stringify(this.products),
                 JSON.stringify(this.productsquantity),
-                JSON.stringify(this.prices),
-                this.total_price,
+                JSON.stringify(this.total_price),
                 this.payment_method,
                 this.address,
-                this.phone_number
+                this.phone_number,
+                this.first_name,
+                this.last_name
+
             ]);
             console.log('Order created successfully.');
         } catch (error) {
@@ -42,14 +47,42 @@ class Order {
                 const orderData = rows[0];
                 return new Order(
                     orderData.id,
+                    orderData.userid,
                     orderData.useremail,
                     JSON.parse(orderData.products),
                     JSON.parse(orderData.productsquantity),
-                    JSON.parse(orderData.prices),
-                    orderData.total_price,
+                    JSON.parse(orderData.total_price),
                     orderData.payment_method,
                     orderData.address,
-                    orderData.phone_number
+                    orderData.phone_number,
+                    orderData.first_name,
+                    orderData.last_name
+                );
+            }
+            return null;
+        } catch (error) {
+            console.error('Error retrieving order:', error);
+            return null;
+        }
+    }
+    static async getOrdersByuserId(userid) {
+        try {
+            const query = 'SELECT * FROM orders WHERE userid = ?';
+            const [rows, fields] = await connection.promise().execute(query, [userid]);
+            if (rows.length > 0) {
+                const orderData = rows[0];
+                return new Order(
+                    orderData.id,
+                    orderData.userid,
+                    orderData.useremail,
+                    JSON.parse(orderData.products),
+                    JSON.parse(orderData.productsquantity),
+                    JSON.parse(orderData.total_price),
+                    orderData.payment_method,
+                    orderData.address,
+                    orderData.phone_number,
+                    orderData.first_name,
+                    orderData.last_name
                 );
             }
             return null;
@@ -60,25 +93,28 @@ class Order {
     }
 
     // Update an order
-    async updateOrder() {
+    static async updateOrder(id, userid, useremail, products, productsquantity, total_price, payment_method, address, phone_number, first_name, last_name) {
         try {
-            const query = 'UPDATE orders SET useremail = ?, products = ?, productsquantity = ?, prices = ?, total_price = ?, payment_method = ?, address = ?, phone_number = ? WHERE id = ?';
+            const query = 'UPDATE orders SET user_id = ?, user_email = ?, products = ?, quantities = ?, total_price = ?, payment_method = ?, address = ?, phone_number = ?, first_name = ?, last_name = ? WHERE id = ?';
             await connection.promise().execute(query, [
-                this.useremail,
-                JSON.stringify(this.products),
-                JSON.stringify(this.productsquantity),
-                JSON.stringify(this.prices),
-                this.total_price,
-                this.payment_method,
-                this.address,
-                this.phone_number,
-                this.id
+                userid,
+                useremail,
+                JSON.stringify(products),
+                JSON.stringify(productsquantity),
+                total_price,
+                payment_method,
+                address,
+                phone_number,
+                first_name,
+                last_name,
+                id
             ]);
             console.log('Order updated successfully.');
         } catch (error) {
             console.error('Error updating order:', error);
         }
     }
+
 
     // Delete an order by ID
     static async deleteOrder(orderId) {
