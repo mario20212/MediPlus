7d1a19139a0dd2bea1e20b335bf6ead0dd9ce55f
Mario Magdy <mariomagdyfa@gmail.com>
2023-12-19 19:29:34 +0200

    added

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,14 +1,16 @@
 const Router=require('express');
 const router=Router();
+const utilitiesController = require('../controllers/utilities-controller');
+
+const Utilites = new utilitiesController();
 
 
 router.get('/', (req,res)=>{
     res.render('schedule');
 });
 
-router.post('/', (req, res) => {
-    const schedule = req.body;
-    
-});  
+router.post('/', (req, res) => Utilites.saveschedule(req,res)); 
+
+
 
 module.exports = router;
