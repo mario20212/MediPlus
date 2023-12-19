168395f8f706560ef4428ac93104173338a26e44
Omarbakary18 <125599045+Omarbakary18@users.noreply.github.com>
2023-12-20 01:39:37 +0200

    added checkAndSendRemainder function

--- models/schedule.js
+++ models/schedule.js
@@ -1,31 +1,84 @@
 const { connection, query } = require('../database/MySQL-connection');
+const System = require('../models/system.js'); // Make sure to provide the correct path to your System class
 
-class schedule
-{
-    
-    constructor(email,usermedicineName,id,dose,whenToTakeValues)
-    {
-        this.user_email= email;
+class Schedule {
+    constructor(email, usermedicineName, id, dose, whenToTakeValues) {
+        this.user_email = email;
         this.medicineName = usermedicineName;
         this.id = id;
         this.dosage = dose;
         this.whenToTakeValues = whenToTakeValues;
     }
-
-    async save()
-    {
-        await query('INSERT INTO schedule (table_id, email, medicineName,dosage,wtt) VALUES (?, ?, ?, ?, ?)', [this.id, this.user_email, this.medicineName, this.dosage, this.whenToTakeValues]);
+    async getAllSchedules(){
+        try {
+            const result = await query('SELECT * FROM schedule');
+            return result;
+        } catch (error) {
+            console.error('Error retrieving schedules:', error);
+            throw error;
+        }
     }
-    
-    
 
-    
+    async save() {
+        // Your save method implementation
+    }
 
+    static async checkAndSendReminder() {
+        const allSchedules = await new Schedule().getAllSchedules();
+        const date = new Date(); // Get the current hour (0-23)
+        const currentHour = date.getHours();
+        const currentMinutes = date.getMinutes();
+        // Check if the current hour matches any of the specified options in whenToTakeValues
+        allSchedules.forEach(schedule => {
+        try{ 
+            if (schedule.whenToTakeValues == "1beforeBreakfast"  && currentHour === 1 && currentMinutes === 37) {
+                // If the current hour is in the specified options, send the email reminder
+                const message = `It's time to take ${this.dosage} pills of your medicine (${this.medicineName}) now.`;
+                
+                const system = new System();
+                system.sendEmail(this.user_email, message);
+            }
+            else if (schedule.whenToTakeValues == "2afterBreakfast" && currentHour === 12) {
+                // If the current hour is in the specified options, send the email reminder
+                const message = `It's time to take ${this.dosage} pills of your medicine (${this.medicineName}) now.`;
+                
+                const system = new System();
+                system.sendEmail(this.user_email, message);
+            }
+            else if (schedule.whenToTakeValues == "3beforeLunch" && currentHour === 15) {
+                // If the current hour is in the specified options, send the email reminder
+                const message = `It's time to take ${this.dosage} pills of your medicine (${this.medicineName}) now.`;
+                
+                const system = new System();
+                system.sendEmail(this.user_email, message);
+            }
+            else if (schedule.whenToTakeValues == "4afterLunch" && currentHour === 18) {
+                // If the current hour is in the specified options, send the email reminder
+                const message = `It's time to take ${this.dosage} pills of your medicine (${this.medicineName}) now.`;
+                
+                const system = new System();
+                system.sendEmail(this.user_email, message);
+            }
+            else if (schedule.whenToTakeValues == "5beforeDinner" && currentHour === 21) {
+                // If the current hour is in the specified options, send the email reminder
+                const message = `It's time to take ${this.dosage} pills of your medicine (${this.medicineName}) now.`;
+                
+                const system = new System();
+                system.sendEmail(this.user_email, message);
+            }
+        }catch (error) {
+            console.error('Error retrieving schedules:', error);
+            throw error;
+        }
+        });
+    }
 
-
-
-
+    static async continuousReminderCheck() {
+        while (true) {
+            await this.checkAndSendReminder();
+            await new Promise(resolve => setTimeout(resolve, 1000)); // Delay for one second
+        }
+    }
 }
 
-
-module.exports = schedule;
+module.exports = Schedule;
